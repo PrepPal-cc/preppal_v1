93e7b72e0c329e796b95ee99b413b6f86eb47dd7
Richard Perez Jr. <richard.p23@icloud.com>
2024-07-29 18:32:26 -0400

    Include OpenAI key in SST deployment

--- stacks/Web.ts
+++ stacks/Web.ts
@@ -1,8 +1,13 @@
-import { StackContext, NextjsSite } from "sst/constructs";
+import { StackContext, NextjsSite, Config } from "sst/constructs";
 
 export function Web({ stack }: StackContext) {
+  const OPENAI_API_KEY = new Config.Secret(stack, "OPENAI_API_KEY");
+
   const site = new NextjsSite(stack, "Site", {
-    path: "."
+    path: ".",
+    environment: {
+      OPENAI_API_KEY: OPENAI_API_KEY.name,
+    },
   });
 
   stack.addOutputs({
